0ed195afd434e2d331eb6d12629099d201961dab
Hammam Alzilitni <hammam@Hammams-MBP.broadband>
2022-02-14 15:54:23 +0000

    implemented second GET endpoint #14

--- controllers/app.controllers.js
+++ controllers/app.controllers.js
@@ -1,4 +1,4 @@
-const { fetchTopics } = require("../models/app.models");
+const { fetchTopics, fetchArticleById } = require("../models/app.models");
 
 exports.getTopics = (req, res, next) => {
   fetchTopics()
@@ -6,5 +6,14 @@ exports.getTopics = (req, res, next) => {
       res.status(200).send({ topics });
     })
     .catch(next)
-    
 };
+
+exports.getArticlesById = (req, res, next) => {
+  const id = req.params.article_id
+  fetchArticleById(id)
+    .then((articles) => {
+      res.status(200).send({ articles });
+    })
+    .catch(next)
+};
+
--- models/app.models.js
+++ models/app.models.js
@@ -1,7 +1,14 @@
 const db = require("../db/connection");
 
 exports.fetchTopics = async () => {
-  const result = await db.query(`SELECT * FROM topics;`)
-  return result.rows
- 
+  const result = await db.query(`SELECT * FROM topics;`);
+  return result.rows;
 };
+
+exports.fetchArticleById = async (id) => {
+
+  
+  const result = await db.query(`SELECT * FROM articles WHERE article_id = $1;`,[id]);
+  return result.rows
+};
+
