3dac93b9e4ece23715839a19aec47b092c17adc1
Hammam Alzilitni <hammam@Hammams-MBP.broadband>
2022-02-14 13:51:49 +0000

    set up test environments and implemented first get endpoint

---
+++ __tests__/app.test.js
@@ -0,0 +1,42 @@
+const request = require("supertest");
+const app = require("../app");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+const db = require("../db/connection");
+
+beforeEach(async () => {
+  await seed(data);
+});
+
+afterAll(async () => {
+  await db.end();
+});
+
+describe("app", () => {
+  describe("GET api/topics", () => {
+    test("status: 200, responds with an array of topic objects", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({body}) => {
+     
+          body.topics.forEach((topic) => {
+            expect(topic).toEqual(
+              expect.objectContaining({
+                slug: expect.any(String),
+                description: expect.any(String),
+              })
+            );
+          });
+        })
+    });
+    test("status: 404, responds with path not found", () => {
+      return request(app).get(
+        "/api/deadend").expect(404).then((result) => {
+          
+          
+        })
+      
+    });
+  });
+});
