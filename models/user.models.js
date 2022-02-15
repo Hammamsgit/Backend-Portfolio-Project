57e47e4c76b512f657cd446d7e9dc28c2fd17440
Hammam Alzilitni <hammam@Hammams-MBP.broadband>
2022-02-15 16:44:35 +0000

    updated the model files by changing async await to promises

--- models/user.models.js
+++ models/user.models.js
@@ -1,7 +1,9 @@
 const db = require("../db/connection")
 
 
-exports.fetchUsers = async ()=>{
-    const users = await db.query("SELECT * FROM users;")
-    return users.rows
+exports.fetchUsers = ()=>{
+    return db.query("SELECT * FROM users;").then(({rows})=>{
+        return rows
+    })
+    
 }
